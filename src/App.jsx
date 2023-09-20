a9490d3ac3595e0fab0da19ecef6800da1cda089
AbdurRahman <arshadhinjr@gmail.com>
2023-09-20 20:41:03 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header"
+
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <h2>Allahu Akbar</h2>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+import React, { useRef, useEffect, useState } from "react";
+
+const Header = () => {
+  const videoRef = useRef(null);
+  const [facingMode, setFacingMode] = useState("user");
+
+  useEffect(() => {
+    const startVideo = async () => {
+      try {
+        const constraints = { video: { facingMode } };
+        const stream = await navigator.mediaDevices.getUserMedia(constraints);
+        videoRef.current.srcObject = stream;
+      } catch (error) {
+        console.error("Error accessing camera:", error);
+      }
+    };
+
+    startVideo();
+  }, [facingMode]);
+
+  const toggleCamera = () => {
+    setFacingMode((prevFacingMode) =>
+      prevFacingMode === "user" ? "environment" : "user"
+    );
+  };
+
+  return (
+    <div>
+      <video style={{ width: "400px", height: "300px", borderRadius: "90%" }} ref={videoRef} autoPlay></video>
+      <button onClick={toggleCamera}>Toggle Camera</button>
+    </div>
+  );
+};
+
+export default Header;
